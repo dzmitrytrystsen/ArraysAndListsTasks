6963f30194f71f94b381a7c5dbeacebb857c4261
Dzmitry Trystsen <dzmitry.trystsen@1pt.com>
2018-08-20 13:19:48 +0300

    Final build

--- ArraysAndListsTasks/Program.cs
+++ ArraysAndListsTasks/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace ArraysAndListsTasks
 {
@@ -6,16 +7,49 @@ namespace ArraysAndListsTasks
     {
         static void Main()
         {
-            Console.WriteLine("Hello, GitHub!");
+            //When you post a message on Facebook, depending on the number of people who like your post, 
+            //Facebook displays different information.
+            //If no one likes your post, it doesn't display anything.
+            //If only one person likes your post, it displays: [Friend's Name] likes your post.
+            //If two people like your post, it displays: [Friend 1] and[Friend 2] like your post.
+            //If more than two people like your post, it displays: 
+            //[Friend 1], [Friend 2] and[Number of Other People] others like your post.
+            //Write a program and continuously ask the user to enter different names, 
+            //until the user presses Enter (without supplying a name). 
+            //Depending on the number of names provided, display a message based on the above pattern.
 
+            var names = new List<string>();
 
+            while (true)
+            {
+                Console.WriteLine("Continue typing names or type 'ENTER' to finish: ");
+                var input = Console.ReadLine();
 
+                if (input.ToUpper() != "ENTER")
+                {
+                    names.Add(input);
+                    continue;
+                }
 
+                else if (names.Count == 1)
+                {
+                    Console.WriteLine("{0} likes your post.", names[0]);
+                }
 
+                else if (names.Count == 2)
+                {
+                    Console.WriteLine("{0} and {1} like your post.", names[0], names[1]);
+                }
 
+                else if (names.Count > 2)
+                {
+                    Console.WriteLine("{0}, {1} and {2} others like your post.", names[0], names[1], names.Count - 2);
+                }
 
-
-            Console.ReadKey();
+                Console.WriteLine("Press any key to restart...");
+                Console.ReadKey();
+                Main();
+            }
         }
     }
 }
