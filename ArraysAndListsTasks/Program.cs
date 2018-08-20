2864835a84b6acfe2ebd40a5d50ec393610d1b70
Dzmitry Trystsen <dzmitry.trystsen@1pt.com>
2018-08-20 11:49:27 +0300

    Add project files.

---
+++ ArraysAndListsTasks/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ArraysAndListsTasks
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
